b9bc750e9acdcb49651d8dad44c163cd1b03a03f
Admiregoogle <53369238+Admiregoogle@users.noreply.github.com>
2024-02-19 13:57:12 +0800

    Update zygisk-ctl.sh

--- module/src/zygisk-ctl.sh
+++ module/src/zygisk-ctl.sh
@@ -2,6 +2,5 @@ MODDIR=${0%/*}/..
 
 export TMP_PATH=/sbin
 [ -d /sbin ] || export TMP_PATH=/debug_ramdisk
-[ ! -d /data/adb/ap ] || export TMP_PATH=/dev/debug_ramdisk
 
 exec $MODDIR/bin/zygisk-ptrace64 ctl $*
